6ff2967048bb221498e7f267010e8dc46fbe3f9b
Rohan2104 <maindarkarrohan@yahoo.com>
2023-07-14 22:59:03 +0530

    added footer code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="card text-center container">
+      <div className="card-header">Featured</div>
+      <div className="card-body">
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">
+          With supporting text below as a natural lead-in to additional content.
+        </p>
+        <a href="#" className="btn btn-primary">
+          Go Somewhere
+        </a>
+      </div>
+      <div className="card-footer text-body-secondary">
+        Developed by RM-Deve's &#169; 2023
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
